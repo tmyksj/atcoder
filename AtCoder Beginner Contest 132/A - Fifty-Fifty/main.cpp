73659705b8df62205000f6b5bd8b4dc514e5a2c8
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-04 08:23:30 +0900

    A - Fifty-Fifty

---
+++ AtCoder Beginner Contest 132/A - Fifty-Fifty/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    vector<int> count(26, 0);
+    for (int i = 0; i < 4; i++) {
+        count[s[i] - 'A']++;
+    }
+
+    int cnt2 = 0;
+    for (int i = 0; i < 26; i++) {
+        if (count[i] == 2) {
+            cnt2++;
+        }
+    }
+
+    cout << (cnt2 == 2 ? "Yes" : "No") << endl;
+}
