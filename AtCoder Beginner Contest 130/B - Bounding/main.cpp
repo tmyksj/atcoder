f58d20c999d7c93b4fb1d77835cbdd802529e78a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-06 08:06:31 +0900

    B - Bounding

---
+++ AtCoder Beginner Contest 130/B - Bounding/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int cnt = 1;
+    for (int i = 0, d = 0; i < n; i++) {
+        int l;
+        cin >> l;
+        d += l;
+
+        if (d <= x) {
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
