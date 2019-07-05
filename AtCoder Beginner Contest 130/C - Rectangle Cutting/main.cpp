241556398b6cabfc1764173f07cc8cc81f6aaa61
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-06 08:51:52 +0900

    C - Rectangle Cutting

---
+++ AtCoder Beginner Contest 130/C - Rectangle Cutting/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long w, h, x, y;
+    cin >> w >> h >> x >> y;
+    cout << w * h / 2.0 << " " << (x == w / 2.0 && y == h / 2.0 ? 1 : 0) << endl;
+}
