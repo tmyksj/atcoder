4aac22115e843c55acb5c2d0cf3ebb3ed2f4f2c1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-05 21:03:40 +0900

    A - Security

---
+++ AtCoder Beginner Contest 131/A - Security/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << (s[0] == s[1] || s[1] == s[2] || s[2] == s[3] ? "Bad" : "Good") << endl;
+}
