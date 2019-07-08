8a9be1bda0fb00d17093b0244f6b9c37b2e1cfd2
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-08 22:03:07 +0900

    C - Dice and Coin

---
+++ AtCoder Beginner Contest 126/C - Dice and Coin/main.cpp
@@ -0,0 +1,20 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    double sum = 0;
+    for (int i = 1; i <= n; i++) {
+        int toss = 0;
+        for (int j = i; j < k; j *= 2) {
+            toss++;
+        }
+        sum += 1.0 / ((1 << toss) * n);
+    }
+
+    cout << setprecision(12) << fixed << sum << endl;
+}
