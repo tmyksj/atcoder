23565ce7394b5c7407887af555d7796709cfb9e6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-13 16:15:42 +0900

    B - Great Ocean View

---
+++ AtCoder Beginner Contest 124/B - Great Ocean View/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for (int i = 0, hmax = 0; i < n; i++) {
+        int h;
+        cin >> h;
+
+        if (hmax <= h) {
+            hmax = h;
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
