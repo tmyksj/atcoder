32d408e66e546bc37e8783cc35d6da268e98fc8a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-14 10:06:52 +0900

    C - Five Transportations

---
+++ AtCoder Beginner Contest 123/C - Five Transportations/main.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, a, b, c, d, e;
+    cin >> n >> a >> b >> c >> d >> e;
+    cout << (n - 1) / min(a, min(b, min(c, min(d, e)))) + 5 << endl;
+}
