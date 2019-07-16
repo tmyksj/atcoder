fbf2258f100603ddbf60d401c56ae3186238e339
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-16 21:00:01 +0900

    B - Can you solve this?

---
+++ AtCoder Beginner Contest 121/B - Can you solve this?/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m, c;
+    cin >> n >> m >> c;
+
+    vector<int> b(m);
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = c;
+        for (int j = 0; j < m; j++) {
+            int a;
+            cin >> a;
+            sum += a * b[j];
+        }
+
+        if (sum > 0) {
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+}
