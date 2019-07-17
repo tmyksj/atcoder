1105ca433552b27213221581754988723708985d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-17 22:02:24 +0900

    B - K-th Common Divisor

---
+++ AtCoder Beginner Contest 120/B - K-th Common Divisor/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, k;
+    cin >> a >> b >> k;
+
+    for (int i = (a < b ? a : b); i >= 1; i--) {
+        if (a % i == 0 && b % i == 0) {
+            k--;
+
+            if (k == 0) {
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+}
