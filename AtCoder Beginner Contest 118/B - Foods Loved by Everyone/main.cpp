dd2713aa12b01325bc67444f057c41345b7ff00e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-19 21:11:48 +0900

    B - Foods Loved by Everyone

---
+++ AtCoder Beginner Contest 118/B - Foods Loved by Everyone/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> cnt(m, 0);
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+
+        for (int j = 0; j < k; j++) {
+            int a;
+            cin >> a;
+            cnt[a - 1]++;
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < m; i++) {
+        if (cnt[i] == n) {
+            sum++;
+        }
+    }
+
+    cout << sum << endl;
+}
