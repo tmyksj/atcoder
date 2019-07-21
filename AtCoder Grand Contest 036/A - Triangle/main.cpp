631e25d853385a8ee51be83b4b673864c65ee479
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-22 08:55:09 +0900

    A - Triangle

---
+++ AtCoder Grand Contest 036/A - Triangle/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long s;
+    cin >> s;
+
+    long long x3 = (s % 1000000000 == 0 ? 0 : 1000000000 - s % 1000000000);
+    long long y3 = (s + x3) / 1000000000;
+    cout << "0 0 1000000000 1 " << x3 << " " << y3 << endl;
+}
