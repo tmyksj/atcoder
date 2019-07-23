7496b678765f1e25de6e5ed716c46e3c80098cce
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-23 18:32:25 +0900

    A - Entrance Examination

---
+++ AtCoder Beginner Contest 117/A - Entrance Examination/main.cpp
@@ -0,0 +1,10 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, x;
+    cin >> t >> x;
+    cout << setprecision(12) << fixed << static_cast<double>(t) / x << endl;
+}
