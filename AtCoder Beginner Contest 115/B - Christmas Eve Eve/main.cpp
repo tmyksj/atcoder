fcc62fb123e4970ed6fd0ad591e3599812eeeb25
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-07-30 14:34:26 +0900

    B - Christmas Eve Eve

---
+++ AtCoder Beginner Contest 115/B - Christmas Eve Eve/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0, max = 0;
+    for (int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+
+        if (max < p) {
+            sum += max;
+            max = p;
+        } else {
+            sum += p;
+        }
+    }
+
+    sum += max / 2;
+    cout << sum << endl;
+}
