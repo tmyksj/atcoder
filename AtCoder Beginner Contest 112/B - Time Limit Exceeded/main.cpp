0ee3472edf4ffa1b28126c8589303069918925ec
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-04 11:47:23 +0900

    B - Time Limit Exceeded

---
+++ AtCoder Beginner Contest 112/B - Time Limit Exceeded/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    int c_min = 1001;
+    for (int i = 0; i < n; i++) {
+        int ci, ti;
+        cin >> ci >> ti;
+
+        if (c_min > ci && t >= ti) {
+            c_min = ci;
+        }
+    }
+
+    if (c_min <= 1000) {
+        cout << c_min << endl;
+    } else {
+        cout << "TLE" << endl;
+    }
+}
