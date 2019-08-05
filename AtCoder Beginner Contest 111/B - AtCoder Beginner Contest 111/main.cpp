e28b59941f3fc7ece72f5d8b0d850315f923650d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-05 20:20:27 +0900

    B - AtCoder Beginner Contest 111

---
+++ AtCoder Beginner Contest 111/B - AtCoder Beginner Contest 111/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 9; i++) {
+        if (n <= 100 * i + 10 * i + i) {
+            cout << 100 * i + 10 * i + i << endl;
+            break;
+        }
+    }
+}
