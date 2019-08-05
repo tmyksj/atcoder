3859372ea6a182ca1c2252c16f34fb8598d55cdf
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-05 20:17:28 +0900

    A - AtCoder Beginner Contest 999

---
+++ AtCoder Beginner Contest 111/A - AtCoder Beginner Contest 999/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < static_cast<int>(s.size()); i++) {
+        cout << (s[i] == '1' ? '9' : '1');
+    }
+    cout << endl;
+}
