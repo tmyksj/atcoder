b43549ce4efaf8d76167892a88edf822458aea13
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-06 20:56:39 +0900

    A - Maximize the Formula

---
+++ AtCoder Beginner Contest 110/A - Maximize the Formula/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << max(10 * a + b + c, max(a + 10 * b + c, a + b + 10 * c)) << endl;
+}
