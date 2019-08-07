a32d95cdaeddc3e81d29dd4b7e44a0df2b29d472
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-08 06:50:07 +0900

    B - Ruined Square

---
+++ AtCoder Beginner Contest 108/B - Ruined Square/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << x2 + y1 - y2
+        << " " << y2 + x2 - x1
+        << " " << x1 + y1 - y2
+        << " " << y1 + x2 - x1
+        << endl;
+}
