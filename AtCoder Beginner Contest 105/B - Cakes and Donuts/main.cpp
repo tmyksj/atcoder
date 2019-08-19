c47b85f294ac290ad806a7a95c108093ceb286c1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-20 06:45:06 +0900

    B - Cakes and Donuts

---
+++ AtCoder Beginner Contest 105/B - Cakes and Donuts/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool exists = false;
+    for (int i = 0; 4 * i <= n; i++) {
+        if ((n - 4 * i) % 7 == 0) {
+            exists = true;
+        }
+    }
+
+    cout << (exists ? "Yes" : "No") << endl;
+}
