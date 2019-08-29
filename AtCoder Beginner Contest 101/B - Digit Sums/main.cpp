f2121bed29945401f876d79c219cc690f4ca43ff
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-29 19:38:23 +0900

    B - Digit Sums

---
+++ AtCoder Beginner Contest 101/B - Digit Sums/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sn = 0;
+    for (int i = n; i > 0; i /= 10) {
+        sn += i % 10;
+    }
+
+    cout << (n % sn == 0 ? "Yes" : "No") << endl;
+}
