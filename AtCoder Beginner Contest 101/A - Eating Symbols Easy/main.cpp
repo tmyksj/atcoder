9b57ca8b9e05410c688e62cfb7e831e69df2f57e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-08-29 19:33:55 +0900

    A - Eating Symbols Easy

---
+++ AtCoder Beginner Contest 101/A - Eating Symbols Easy/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    cout << (s[0] == '+' ? 1 : -1)
+        + (s[1] == '+' ? 1 : -1)
+        + (s[2] == '+' ? 1 : -1)
+        + (s[3] == '+' ? 1 : -1) << endl;
+}
