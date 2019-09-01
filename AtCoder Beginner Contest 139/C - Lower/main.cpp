e008c9c81185b8739d2493d6acf4e2a6d2fb592b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-01 22:42:29 +0900

    C - Lower

---
+++ AtCoder Beginner Contest 139/C - Lower/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> h(n);
+    for (int i = 0; i < n; i++) {
+        cin >> h[i];
+    }
+
+    int res = 0;
+    for (int i = 0, r = 0; i < n - 1; i++) {
+        if (h[i] >= h[i + 1]) {
+            r++;
+        } else {
+            r = 0;
+        }
+
+        if (res < r) {
+            res = r;
+        }
+    }
+
+    cout << res << endl;
+}
