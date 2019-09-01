36bc681d3d8d2ab41f7f4b2213ea8798ec94f74c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-01 22:42:21 +0900

    A - Tenki

---
+++ AtCoder Beginner Contest 139/A - Tenki/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << (s[0] == t[0] ? 1 : 0) + (s[1] == t[1] ? 1 : 0) + (s[2] == t[2] ? 1 : 0) << endl;
+}
