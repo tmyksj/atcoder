7d8f2a67c555de77380a199a89f3e873454f7482
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-03 08:47:05 +0900

    B - Exponential

---
+++ AtCoder Beginner Contest 097/B - Exponential/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+
+    int res = 1;
+    for (int i = 2; i <= x; i++) {
+        for (int j = i * i; j <= x; j *= i) {
+            if (res < j) {
+                res = j;
+            }
+        }
+    }
+
+    cout << res << endl;
+}
