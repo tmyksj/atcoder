71d7da7e6fd18e98da44ce511c49ae53de3459ca
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-04 20:26:32 +0900

    A - Irreversible operation

---
+++ AtCoder Grand Contest 029/A - Irreversible operation/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    long long res = 0;
+    for (int i = 0, l = 0; i < static_cast<int>(s.size()); i++) {
+        if (s[i] == 'W') {
+            res += i - l;
+            l++;
+        }
+    }
+
+    cout << res << endl;
+}
