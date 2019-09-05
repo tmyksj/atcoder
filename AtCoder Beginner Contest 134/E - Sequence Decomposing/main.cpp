415785a2ef0303052377cf9349377051fb2728c5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-05 09:13:52 +0900

    E - Sequence Decomposing

---
+++ AtCoder Beginner Contest 134/E - Sequence Decomposing/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    multiset<int> b;
+    b.insert(a[n - 1]);
+
+    for (int i = n - 2; i >= 0; i--) {
+        multiset<int>::iterator ite = b.upper_bound(a[i]);
+        if (ite != b.end()) {
+            b.erase(ite);
+        }
+
+        b.insert(a[i]);
+    }
+
+    cout << b.size() << endl;
+}
