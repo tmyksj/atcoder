842717c47f3f05c75475a88947cd12900fd7e44d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-07 08:28:28 +0900

    B - Bitter Alchemy

---
+++ AtCoder Beginner Contest 095/B - Bitter Alchemy/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int m_min = 1000;
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m;
+
+        x -= m;
+        if (m_min > m) {
+            m_min = m;
+        }
+    }
+
+    cout << n + x / m_min << endl;
+}
