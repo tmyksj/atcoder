a93db10afac391129f34fe00fd3f36ca0c80d585
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-07 22:41:09 +0900

    C - Maximal Value

---
+++ AtCoder Beginner Contest 140/C - Maximal Value/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> b(n + 1, 1000000000);
+    for (int i = 1; i <= n - 1; i++) {
+        cin >> b[i];
+    }
+
+    int res = 0;
+    for (int i = 1; i <= n; i++) {
+        res += min(b[i - 1], b[i]);
+    }
+
+    cout << res << endl;
+}
