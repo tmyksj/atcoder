fcc159a90ff9f9e010145db7488a9a843892a0dc
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-09 07:47:44 +0900

    A - abc of ABC

---
+++ AtCoder Beginner Contest 093/A - abc of ABC/main.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    cout << (s == "abc" ? "Yes" : "No") << endl;
+}
