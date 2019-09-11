7f8111034eb0b97c4d918f5344c3260193470ce1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-12 08:03:22 +0900

    B - Palindromic Numbers

---
+++ AtCoder Beginner Contest 090/B - Palindromic Numbers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int res = 0;
+    for (int i = a; i <= b; i++) {
+        if (i % 10 == i / 10000 && (i / 10) % 10 == (i / 1000) % 10) {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
