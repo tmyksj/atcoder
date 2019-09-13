c7dff97f543bea7117a8a17cfc2658c0a9cea2b1
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-13 18:39:13 +0900

    B - Hina Arare

---
+++ AtCoder Beginner Contest 089/B - Hina Arare/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string res = "Three";
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        if (s == "Y") {
+            res = "Four";
+        }
+    }
+
+    cout << res << endl;
+}
