690506a08424f7cc79174bc14c275655d8c5a13f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-15 22:45:16 +0900

    C - Attack Survival

---
+++ AtCoder Beginner Contest 141/C - Attack Survival/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, k, q;
+    cin >> n >> k >> q;
+
+    vector<int> cnt(n, 0);
+    for (int i = 0; i < q; i++) {
+        int a;
+        cin >> a;
+        cnt[a - 1]++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << (k - (q - cnt[i]) > 0 ? "Yes" : "No") << endl;
+    }
+}
