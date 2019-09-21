c7c539603ae210d8290b61b0d1a2357f8ee535a9
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-22 08:37:16 +0900

    A - 01 Matrix

---
+++ AtCoder Grand Contest 038/A - 01 Matrix/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h, w, a, b;
+    cin >> h >> w >> a >> b;
+
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cout << ((i < b && j < a) != (b <= i && a <= j) ? "0" : "1");
+        }
+        cout << endl;
+    }
+}
