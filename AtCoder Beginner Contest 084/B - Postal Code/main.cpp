5b439a4080d8ed9446f03fa652134df5fa051b61
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-23 08:53:23 +0900

    B - Postal Code

---
+++ AtCoder Beginner Contest 084/B - Postal Code/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    string s;
+    cin >> a >> b >> s;
+
+    bool res = true;
+    for (int i = 0; i < a + b + 1; i++) {
+        if ((i == a && s[i] != '-') || (i != a && s[i] == '-')) {
+            res = false;
+        }
+    }
+
+    cout << (res == 1 ? "Yes" : "No") << endl;
+}
