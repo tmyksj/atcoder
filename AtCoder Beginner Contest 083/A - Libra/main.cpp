fd30c273b6fd572ba1b2da53ade36bb8d4623806
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-23 18:54:54 +0900

    A - Libra

---
+++ AtCoder Beginner Contest 083/A - Libra/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (a + b < c + d) {
+        cout << "Right" << endl;
+    } else if (a + b == c + d) {
+        cout << "Balanced" << endl;
+    } else {
+        cout << "Left" << endl;
+    }
+}
