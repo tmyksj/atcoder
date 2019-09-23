461fdc450d7afe6b3264395df9bcb6a5115dffb5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-23 19:24:41 +0900

    D - Wide Flip

---
+++ AtCoder Beginner Contest 083/D - Wide Flip/main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int n = static_cast<int>(s.size());
+
+    int res = n;
+    for (int i = 1; i < n; i++) {
+        if (s[i - 1] != s[i]) {
+            res = min(res, max(i, n - i));
+        }
+    }
+
+    cout << res << endl;
+}
