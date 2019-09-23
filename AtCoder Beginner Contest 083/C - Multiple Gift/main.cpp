2d0a259c5ebe40f0b4989e8a8a0717841d38e19f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-23 19:03:42 +0900

    C - Multiple Gift

---
+++ AtCoder Beginner Contest 083/C - Multiple Gift/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long x, y;
+    cin >> x >> y;
+
+    int res = 0;
+    for (long long i = x; i <= y; i *= 2) {
+        res++;
+    }
+
+    cout << res << endl;
+}
