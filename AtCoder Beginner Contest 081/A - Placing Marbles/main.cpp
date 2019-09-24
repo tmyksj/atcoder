a290b82e4f30c601f95706c18c7d15ae7ee5a417
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-25 08:11:03 +0900

    A - Placing Marbles

---
+++ AtCoder Beginner Contest 081/A - Placing Marbles/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int res = 0;
+    if (s[0] == '1') { res++; }
+    if (s[1] == '1') { res++; }
+    if (s[2] == '1') { res++; }
+
+    cout << res << endl;
+}
