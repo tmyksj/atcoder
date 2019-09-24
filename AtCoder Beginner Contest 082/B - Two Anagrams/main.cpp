40b20fb151c94c47fccb872cc2332c06d8855436
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-24 20:08:53 +0900

    B - Two Anagrams

---
+++ AtCoder Beginner Contest 082/B - Two Anagrams/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end(), greater<char>());
+
+    cout << (s < t ? "Yes" : "No") << endl;
+}
