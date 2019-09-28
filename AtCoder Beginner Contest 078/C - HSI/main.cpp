907b0bc1fbfc58e631d55ab86ffb2bd74306bda5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-29 08:14:54 +0900

    C - HSI

---
+++ AtCoder Beginner Contest 078/C - HSI/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int p = 1;
+    for (int i = 0; i < m; i++) {
+        p *= 2;
+    }
+
+    cout << 100 * (n - m) * p + 1900 * m * p << endl;
+}
