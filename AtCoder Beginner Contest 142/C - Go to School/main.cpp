7be8e6bc9dafd19a54cd134996c6326f88f10d05
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-28 22:43:00 +0900

    C - Go to School

---
+++ AtCoder Beginner Contest 142/C - Go to School/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> a(n, make_pair(0, 0));
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    }
+
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i < n; i++) {
+        cout << (i == 0 ? "" : " ") << a[i].second;
+    }
+    cout << endl;
+}
