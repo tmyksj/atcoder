29a97c44c0094ef305f3872ba6502cb4e222a2da
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-28 22:42:53 +0900

    B - Roller Coaster

---
+++ AtCoder Beginner Contest 142/B - Roller Coaster/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int h;
+        cin >> h;
+
+        if (h >= k) {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
