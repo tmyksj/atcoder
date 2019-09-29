d0dff9b78746362478942b3c54f7f8c52676da81
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-30 08:09:55 +0900

    A - Rotation

---
+++ AtCoder Beginner Contest 077/A - Rotation/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string c0, c1;
+    cin >> c0 >> c1;
+    cout << (c0[0] == c1[2] && c0[1] == c1[1] && c0[2] == c1[0] ? "YES" : "NO") << endl;
+}
