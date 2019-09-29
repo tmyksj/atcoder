8e0aa285bc073b89f8fb20c68f3b2a4aae55e38e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-09-30 08:14:29 +0900

    B - Around Square

---
+++ AtCoder Beginner Contest 077/B - Around Square/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long r = 1;
+    while ((r + 1) * (r + 1) <= n) {
+        r++;
+    }
+
+    cout << r * r << endl;
+}
