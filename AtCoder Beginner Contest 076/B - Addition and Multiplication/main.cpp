c7c1cd91090e9b8b6aca494703ef8ead74aa2a9b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-01 21:14:58 +0900

    B - Addition and Multiplication

---
+++ AtCoder Beginner Contest 076/B - Addition and Multiplication/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int res = 1;
+    for (int i = 0; i < n; i++) {
+        res = min(2 * res, res + k);
+    }
+
+    cout << res << endl;
+}
