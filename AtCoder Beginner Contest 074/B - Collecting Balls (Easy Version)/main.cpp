6d71c33749544557e8f9cc2a6549c930c4215324
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-05 08:06:31 +0900

    B - Collecting Balls (Easy Version)

---
+++ AtCoder Beginner Contest 074/B - Collecting Balls (Easy Version)/main.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int abs(int a) {
+    return a >= 0 ? a : -a;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        res += min(2 * x, 2 * abs(x - k));
+    }
+
+    cout << res << endl;
+}
