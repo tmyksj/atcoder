b7c7c9927420a41fe74263594c54a1c2d4d98a66
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-07 07:59:08 +0900

    D - Derangement

---
+++ AtCoder Beginner Contest 072/D - Derangement/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> p(n);
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (p[i] == i + 1) {
+            res++;
+            i++;
+        }
+    }
+
+    cout << res << endl;
+}
