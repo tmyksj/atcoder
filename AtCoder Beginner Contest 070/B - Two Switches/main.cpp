4c37703e7f3c6eee913eea79743e4ecc713a5fc0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-09 12:44:45 +0900

    B - Two Switches

---
+++ AtCoder Beginner Contest 070/B - Two Switches/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << max(0, min(b, d) - max(a, c)) << endl;
+}
