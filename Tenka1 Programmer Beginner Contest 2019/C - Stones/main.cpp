af3f667774c5f86137b0e89e26b3cc48afc2d951
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-15 11:08:25 +0900

    C - Stones

---
+++ Tenka1 Programmer Beginner Contest 2019/C - Stones/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int wl = 0, wr = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '.') {
+            wr++;
+        }
+    }
+
+    int res = wr;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '.') {
+            wl++;
+            wr--;
+        }
+
+        res = min(res, (i - wl + 1) + wr);
+    }
+
+    cout << res << endl;
+}
