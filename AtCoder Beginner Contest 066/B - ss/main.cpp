31d8a697d158f4a661dd582026f74b151fbd6f6b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-18 07:49:55 +0900

    B - ss

---
+++ AtCoder Beginner Contest 066/B - ss/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int res = 0;
+    for (int i = (s.size() - 1) / 2; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (s[j] != s[i + j]) {
+                break;
+            } else if (j == i - 1) {
+                res = 2 * i;
+                goto out;
+            }
+        }
+    }
+
+out:
+    cout << res << endl;
+}
