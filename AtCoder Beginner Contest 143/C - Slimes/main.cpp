565c83a89d716c6a3e781b5287749c0419215cab
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-19 22:42:24 +0900

    C - Slimes

---
+++ AtCoder Beginner Contest 143/C - Slimes/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int res = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[i - 1] != s[i]) {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
