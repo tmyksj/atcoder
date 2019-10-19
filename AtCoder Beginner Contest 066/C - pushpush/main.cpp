72440bc153d39217b6904f504f90ee4a99d8b15b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-19 09:21:38 +0900

    C - pushpush

---
+++ AtCoder Beginner Contest 066/C - pushpush/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cout << a[n - 1];
+    for (int i = n - 3, d = -2; i < n; i += d) {
+        if (i < 0) {
+            i = (i == -2 ? -1 : -2);
+            d = 2;
+            continue;
+        }
+
+        cout << " " << a[i];
+    }
+    cout << endl;
+}
