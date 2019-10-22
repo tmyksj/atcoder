2962db79482dc2f3a863adb4c8a94c38eea69591
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-23 08:19:05 +0900

    B - Trained?

---
+++ AtCoder Beginner Contest 065/B - Trained?/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int res = -1;
+    vector<bool> trained(n, false);
+    for (int i = 0, r = 0; !trained[i]; i = a[i] - 1, r++) {
+        trained[i] = true;
+        if (i == 1) {
+            res = r;
+            break;
+        }
+    }
+
+    cout << res << endl;
+}
