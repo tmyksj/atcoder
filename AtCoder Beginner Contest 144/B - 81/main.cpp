0637e90fb49444b80563a82fc7f9abddcd495524
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-27 22:46:06 +0900

    B - 81

---
+++ AtCoder Beginner Contest 144/B - 81/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool res = false;
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
+            if (i * j == n) {
+                res = true;
+            }
+        }
+    }
+
+    cout << (res ? "Yes" : "No") << endl;
+}
