ca3972a6a743c2a15e5bd1e32c473c95a9f7c9b5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-29 07:43:05 +0900

    A - Restricted

---
+++ AtCoder Beginner Contest 063/A - Restricted/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a + b < 10) {
+        cout << a + b << endl;
+    } else {
+        cout << "error" << endl;
+    }
+}
