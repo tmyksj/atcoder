2ff14e5d5a4b281df4966918154fa56ca9e4d5b0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-30 07:54:39 +0900

    A - Grouping

---
+++ AtCoder Beginner Contest 062/A - Grouping/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int group[] = { 0, 1, 2, 1, 3, 1, 3, 1, 1, 3, 1, 3, 1 };
+    int x, y;
+    cin >> x >> y;
+    cout << (group[x] == group[y] ? "Yes" : "No") << endl;
+}
