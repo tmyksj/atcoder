169a206566ac7471d47fe4fede8f883bed0d2c6b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-01 07:51:17 +0900

    A - Shiritori

---
+++ AtCoder Beginner Contest 060/A - Shiritori/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a, b, c;
+    cin >> a >> b >> c;
+    cout << (a[a.size() - 1] == b[0] && b[b.size() - 1] == c[0] ? "YES" : "NO") << endl;
+}
