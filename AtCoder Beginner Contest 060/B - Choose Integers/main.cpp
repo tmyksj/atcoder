96e9d6c018b45c98f0c1dfd1930d7015a5a926c6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-01 07:58:17 +0900

    B - Choose Integers

---
+++ AtCoder Beginner Contest 060/B - Choose Integers/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    vector<bool> mod(b, false);
+    for (int i = 1; !mod[i * a % b]; i++) {
+        mod[i * a % b] = true;
+    }
+
+    cout << (mod[c] ? "YES" : "NO") << endl;
+}
