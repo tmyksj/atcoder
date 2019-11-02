50fd7633cced24fc45297a7267248024228c6e67
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-02 09:01:55 +0900

    A - Three-letter acronym

---
+++ AtCoder Beginner Contest 059/A - Three-letter acronym/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+    cout << static_cast<char>(s1[0] - 'a' + 'A')
+            << static_cast<char>(s2[0] - 'a' + 'A')
+            << static_cast<char>(s3[0] - 'a' + 'A') << endl;
+}
