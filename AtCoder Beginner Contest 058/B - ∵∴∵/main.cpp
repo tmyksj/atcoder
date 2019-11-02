2b3529113a569276aa377dee487b269b4093ba96
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-02 20:59:32 +0900

    B - ∵∴∵

---
+++ AtCoder Beginner Contest 058/B - ∵∴∵/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string o, e;
+    cin >> o >> e;
+
+    for (int i = 0; i < static_cast<int>(o.size()); i++) {
+        cout << o[i];
+        if (i < static_cast<int>(e.size())) {
+            cout << e[i];
+        }
+    }
+    cout << endl;
+}
