f389a6f97e0897e33ac86086e65684b5494aaf76
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-05 19:26:40 +0900

    C - Go Home

---
+++ AtCoder Beginner Contest 056/C - Go Home/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long x;
+    cin >> x;
+
+    long long left = 0, right = 2 * x;
+    while (left < right) {
+        long long mid = left + (right - left) / 2;
+
+        if (mid * (mid + 1) >= 2 * x && right == mid) {
+            break;
+        } else if (mid * (mid + 1) >= 2 * x) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    cout << left << endl;
+}
