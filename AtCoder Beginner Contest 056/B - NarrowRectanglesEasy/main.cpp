c1f5b8d03ea7c9fcf1f561c1e1d3d54fd5b802d2
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-05 17:44:47 +0900

    B - NarrowRectanglesEasy

---
+++ AtCoder Beginner Contest 056/B - NarrowRectanglesEasy/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int w, a, b;
+    cin >> w >> a >> b;
+
+    int ab_abs = a > b ? a - b : b - a;
+    cout << (ab_abs <= w ? 0 : ab_abs - w) << endl;
+}
