caa4fc22d7b404803b075816cf6c7a6ae04978e6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-06 22:11:32 +0900

    B - Training Camp

---
+++ AtCoder Beginner Contest 055/B - Training Camp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long res = 1;
+    for (long long i = 2; i <= n; i++) {
+        res *= i;
+        res %= 1000000007;
+    }
+
+    cout << res << endl;
+}
