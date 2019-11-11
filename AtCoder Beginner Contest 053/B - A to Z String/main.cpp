bc8c445b2c697d2313d4b9aaf1b87cd377f27562
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-11 10:23:06 +0900

    B - A to Z String

---
+++ AtCoder Beginner Contest 053/B - A to Z String/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int left = 0;
+    while (s[left] != 'A') {
+        left++;
+    }
+
+    int right = s.size() - 1;
+    while (s[right] != 'Z') {
+        right--;
+    }
+
+    cout << (right - left + 1) << endl;
+}
