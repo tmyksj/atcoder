d27dddac27d3dc740052413f817952b9745c0b12
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-18 08:20:27 +0900

    B - たてなが / Thin

---
+++ AtCoder Beginner Contest 049/B - たてなが / Thin/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int h, w;
+    cin >> h >> w;
+
+    vector<string> c(h);
+    for (int i = 0; i < h; i++) {
+        cin >> c[i];
+    }
+
+    for (int i = 0; i < 2 * h; i++) {
+        cout << c[i / 2] << endl;
+    }
+}
