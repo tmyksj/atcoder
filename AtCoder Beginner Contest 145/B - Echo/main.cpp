05f4265d0fe76c42b7c47766e7e5f8e9d06e6a54
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-18 14:14:15 +0900

    B - Echo

---
+++ AtCoder Beginner Contest 145/B - Echo/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    bool res = (n % 2 == 0);
+    for (int i = 0; i < n / 2 && res; i++) {
+        res = (s[i] == s[i + n / 2]);
+    }
+
+    cout << (res ? "Yes" : "No") << endl;
+}
