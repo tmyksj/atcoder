0e169908c3d84e1739a78f90797eb5b1b3b88c99
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-24 22:40:25 +0900

    B - ROT N

---
+++ AtCoder Beginner Contest 146/B - ROT N/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 0; i < static_cast<int>(s.size()); i++) {
+        s[i] = ((s[i] - 'A' + n) % 26) + 'A';
+    }
+
+    cout << s << endl;
+}
