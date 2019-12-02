ad2d518f459e1ade6c7d98944f3c09e2f7eb83db
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-02 09:05:23 +0900

    F - Interval Running

---
+++ 三井住友信託銀行プログラミングコンテスト2019/F - Interval Running/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long t1, t2, a1, a2, b1, b2;
+    cin >> t1 >> t2 >> a1 >> a2 >> b1 >> b2;
+
+    if (a1 > b1) {
+        swap(a1, b1);
+        swap(a2, b2);
+    }
+
+    if (a1 == b1 || t1 * a1 + t2 * a2 == t1 * b1 + t2 * b2) {
+        cout << "infinity" << endl;
+    } else if (t1 * a1 + t2 * a2 < t1 * b1 + t2 * b2) {
+        cout << "0" << endl;
+    } else {
+        long long d1 = t1 * b1 - t1 * a1;
+        long long d2 = (t1 * a1 + t2 * a2) - (t1 * b1 + t2 * b2);
+        cout << 2 * (d1 / d2) - (d1 % d2 == 0 ? 1 : 0) + 1 << endl;
+    }
+}
