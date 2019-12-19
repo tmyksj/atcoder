5f95da84f7f55c44d4f9ecc5eb4007e8684c4d5d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-20 08:07:30 +0900

    B - □□□□□

---
+++ AtCoder Beginner Contest 040/B - □□□□□/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int res = 1000000000;
+    for (int i = 1; i * i <= n; i++) {
+        if (res > (n / i) - i + n - (n / i * i)) {
+            res = (n / i) - i + n - (n / i * i);
+        }
+    }
+
+    cout << res << endl;
+}
