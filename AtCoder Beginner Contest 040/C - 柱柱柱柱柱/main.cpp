aae9e8bcbdab0f0039b01896e4b46423c97f8343
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-21 08:36:48 +0900

    C - 柱柱柱柱柱

---
+++ AtCoder Beginner Contest 040/C - 柱柱柱柱柱/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n + 2, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<long long> dp(n + 2, 1LL << 61);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        dp[i + 1] = min(dp[i + 1], dp[i] + max(a[i], a[i + 1]) - min(a[i], a[i + 1]));
+        dp[i + 2] = min(dp[i + 2], dp[i] + max(a[i], a[i + 2]) - min(a[i], a[i + 2]));
+    }
+
+    cout << dp[n - 1] << endl;
+}
