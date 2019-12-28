bc27977edea35a1577794d8a3256f790977fe232
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-28 10:34:37 +0900

    E - Double Factorial

---
+++ AtCoder Beginner Contest 148/E - Double Factorial/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        long long res = 0;
+        for (long long i = 10; n / i > 0; i *= 5) {
+            res += n / i;
+        }
+        cout << res << endl;
+    } else {
+        cout << "0" << endl;
+    }
+}
