ce7e5753355eaf60a44ac911aa18dadaa31733cb
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-28 10:11:43 +0900

    D - Brick Break

---
+++ AtCoder Beginner Contest 148/D - Brick Break/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int res = n;
+    for (int i = 0, j = 1; i < n; i++) {
+        int a;
+        cin >> a;
+
+        if (a == j) {
+            res--;
+            j++;
+        }
+    }
+
+    cout << (res < n ? res : -1) << endl;
+}
