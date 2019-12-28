0925def64c7b47dd38b770c80c2b0bc6163f3ace
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-28 10:02:22 +0900

    A - Round One

---
+++ AtCoder Beginner Contest 148/A - Round One/main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b) {
+        swap(a, b);
+    }
+
+    if (a == 1 && b == 2) {
+        cout << "3" << endl;
+    } else if (a == 1 && b == 3) {
+        cout << "2" << endl;
+    } else {
+        cout << "1" << endl;
+    }
+}
