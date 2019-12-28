8082b5a05b355bbc81ccce20d06791312b861f43
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-28 10:03:50 +0900

    B - Strings with the Same Length

---
+++ AtCoder Beginner Contest 148/B - Strings with the Same Length/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string s, t;
+    cin >> n >> s >> t;
+
+    for (int i = 0; i < n; i++) {
+        cout << s[i] << t[i];
+    }
+    cout << endl;
+}
