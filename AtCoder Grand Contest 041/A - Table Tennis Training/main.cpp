f1f29e718a96727c14d8dd9029cab40e189ae861
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-29 10:36:38 +0900

    A - Table Tennis Training

---
+++ AtCoder Grand Contest 041/A - Table Tennis Training/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, a, b;
+    cin >> n >> a >> b;
+
+    if ((b - a) % 2 == 0) {
+        cout << (b - a) / 2 << endl;
+    } else {
+        cout << min(a + (b - a - 1) / 2, n - b + 1 + (n - a - (n - b + 1)) / 2) << endl;
+    }
+}
