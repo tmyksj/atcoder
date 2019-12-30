f67a529489b72e9903fdf45ea1470a54ebf11e25
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-12-31 08:41:49 +0900

    C - 単調増加

---
+++ AtCoder Beginner Contest 038/C - 単調増加/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long res = 0;
+    for (int i = 0, j = 0, k = 1; i < n; i++, k++) {
+        int a;
+        cin >> a;
+
+        if (j >= a) {
+            k = 1;
+        }
+
+        j = a;
+        res += k;
+    }
+
+    cout << res << endl;
+}
