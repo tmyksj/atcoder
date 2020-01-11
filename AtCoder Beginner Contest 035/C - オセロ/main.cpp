4ddbaf7e68ad304b60ed7feaf69f4483fd0af352
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-11 10:00:43 +0900

    C - オセロ

---
+++ AtCoder Beginner Contest 035/C - オセロ/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> s(n + 1, 0);
+    for (int i = 0; i < q; i++) {
+        int l, r;
+        cin >> l >> r;
+        s[l - 1]++;
+        s[r]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << s[i] % 2;
+        s[i + 1] += s[i];
+    }
+    cout << endl;
+}
