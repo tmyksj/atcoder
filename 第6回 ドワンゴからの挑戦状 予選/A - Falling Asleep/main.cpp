bac9d834ec8c1061066f588814c799520cd53523
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-12 11:28:14 +0900

    A - Falling Asleep

---
+++ 第6回 ドワンゴからの挑戦状 予選/A - Falling Asleep/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> s(n);
+    vector<int> t(n);
+    for (int i = 0; i < n; i++) {
+        cin >> s[i] >> t[i];
+    }
+
+    string x;
+    cin >> x;
+
+    int res = 0;
+    for (int i = 0, j = 0; i < n; i++) {
+        res += j * t[i];
+        if (x == s[i]) {
+            j = 1;
+        }
+    }
+
+    cout << res << endl;
+}
