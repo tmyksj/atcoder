53e57d3e44487414a3a79ffbf2f9f5b29370d17b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-19 09:33:58 +0900

    C - Subarray Sum

---
+++ キーエンス プログラミング コンテスト 2020/C - Subarray Sum/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, k, s;
+    cin >> n >> k >> s;
+
+    for (int i = 0; i < k; i++) {
+        cout << s << (i < n - 1 ? " " : "");
+    }
+
+    for (int i = k; i < n; i++) {
+        cout << (s <= 100000 ? 100001 : 1) << (i < n - 1 ? " " : "");
+    }
+
+    cout << endl;
+}
