25210e674d49739c88d76c0fca4fcbfb3a8498f9
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-19 22:34:15 +0900

    B - Comparing Strings

---
+++ AtCoder Beginner Contest 152/B - Comparing Strings/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a < b) {
+        for (int i = 0; i < b; i++) {
+            cout << a;
+        }
+    } else {
+        for (int i = 0; i < a; i++) {
+            cout << b;
+        }
+    }
+    cout << endl;
+}
