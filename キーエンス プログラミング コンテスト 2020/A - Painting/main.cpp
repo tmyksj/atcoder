e23c4851c82d7274bf48690c196b3074a3312750
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-19 09:08:48 +0900

    A - Painting

---
+++ キーエンス プログラミング コンテスト 2020/A - Painting/main.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h, w, n;
+    cin >> h >> w >> n;
+
+    int res = h * w;
+    for (int i = 0; i <= h; i++) {
+        for (int j = 0; j <= w; j++) {
+            if (h * w - (h - i) * (w - j) >= n) {
+                res = min(res, i + j);
+            }
+        }
+    }
+
+    cout << res << endl;
+}
