18175c80ab9658f2f6b0df59e06598c799d915d5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-22 15:17:04 +0900

    E - Change a Little Bit

---
+++ AtCoder Beginner Contest 150/E - Change a Little Bit/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> c(n);
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+
+    sort(c.begin(), c.end(), greater<long long>());
+
+    int mod = 1000000007;
+    long long res = 0;
+    for (int i = 0; i < n; i++) {
+        res += c[i] * (i + 2);
+        res %= mod;
+    }
+
+    for (int i = 1; i < n; i++) {
+        res *= 4;
+        res %= mod;
+    }
+
+    cout << res << endl;
+}
