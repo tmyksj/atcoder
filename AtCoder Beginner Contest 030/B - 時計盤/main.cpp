49d43810f6d585f02285a8148ae974a3c05356d5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-30 08:36:09 +0900

    B - 時計盤

---
+++ AtCoder Beginner Contest 030/B - 時計盤/main.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    double s = 30 * (n % 12) + 0.5 * m;
+    double l = 6 * m;
+
+    cout << min(max(s, l) - min(s, l), min(s, l) - max(s, l) + 360) << endl;
+}
