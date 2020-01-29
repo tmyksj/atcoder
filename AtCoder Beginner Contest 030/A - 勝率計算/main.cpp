dc768486a9627a1b49543e13edf3875bfba59ac9
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-29 20:04:38 +0900

    A - 勝率計算

---
+++ AtCoder Beginner Contest 030/A - 勝率計算/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (b / a > d / c ? "TAKAHASHI" : (b / a == d / c ? "DRAW" : "AOKI")) << endl;
+}
