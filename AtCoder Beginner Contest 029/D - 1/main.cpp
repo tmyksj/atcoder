c82b10c2a3a13f47cd07ede291741c6889a1035f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-03 21:35:23 +0900

    D - 1

---
+++ AtCoder Beginner Contest 029/D - 1/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long res = 0;
+    for (long long i = 1, j = 10; i <= 10; i++, j *= 10) {
+        res += (n / j) * (j / 10);
+        res += min(max(0LL, n % j - j / 10 + 1), j / 10);
+    }
+
+    cout << res << endl;
+}
