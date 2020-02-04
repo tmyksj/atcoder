2286c64564983148aec9c08e3b3f523403edef27
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-04 17:55:15 +0900

    D - 乱数生成

---
+++ AtCoder Beginner Contest 028/D - 乱数生成/main.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+    cout << setprecision(12) << fixed
+            << (6.0 * (k - 1) * (n - k)
+                    + 3.0 * (k - 1)
+                    + 3.0 * (n - k)
+                    + 1.0) / (n * n * n) << endl;
+}
