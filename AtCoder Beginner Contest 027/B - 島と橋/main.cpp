7dbedb59ab4e482021b2c8ccb7e8953341c83a49
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-05 19:55:49 +0900

    B - 島と橋

---
+++ AtCoder Beginner Contest 027/B - 島と橋/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int a_sum = 0;
+    for (int i = 0; i < n; i++) {
+        a_sum += a[i];
+    }
+
+    int res = -1;
+    if (a_sum % n == 0) {
+        res = 0;
+        for (int i = 0, j = 0, k = -1; i < n; i++, res++) {
+            j += a[i];
+            if (j == (i - k) * a_sum / n) {
+                j = 0;
+                k = i;
+                res--;
+            }
+        }
+    }
+
+    cout << res << endl;
+}
