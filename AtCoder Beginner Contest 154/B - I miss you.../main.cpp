c310ccf8b3e5739ca727133df4970ed13cc64529
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-09 22:44:10 +0900

    B - I miss you...

---
+++ AtCoder Beginner Contest 154/B - I miss you.../main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < static_cast<int>(s.size()); i++) {
+        cout << "x";
+    }
+    cout << endl;
+}
