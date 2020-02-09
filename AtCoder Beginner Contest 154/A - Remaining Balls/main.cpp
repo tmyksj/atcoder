0f64e2332779ed711da8f0d228c7ea74d6a562da
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-09 22:44:07 +0900

    A - Remaining Balls

---
+++ AtCoder Beginner Contest 154/A - Remaining Balls/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s, t, u;
+    int a, b;
+    cin >> s >> t >> a >> b >> u;
+
+    if (u == s) {
+        cout << a - 1 << " " << b << endl;
+    } else {
+        cout << a << " " << b - 1 << endl;
+    }
+}
