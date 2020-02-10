278cef4d4170d2e398caccbfd2600a9e6df2866a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-10 21:23:09 +0900

    A - 25個の文字列

---
+++ AtCoder Beginner Contest 025/A - 25個の文字列/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    int n;
+    cin >> s >> n;
+    cout << s[(n - 1) / 5] << s[(n - 1) % 5] << endl;
+}
