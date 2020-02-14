b440979e4200f371c8b9f34fdf806558f5fd04b4
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-14 20:31:51 +0900

    B - 自動ドア

---
+++ AtCoder Beginner Contest 024/B - 自動ドア/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int res = t;
+    for (int i = 1; i < n; i++) {
+        res += min(t, a[i] - a[i - 1]);
+    }
+
+    cout << res << endl;
+}
