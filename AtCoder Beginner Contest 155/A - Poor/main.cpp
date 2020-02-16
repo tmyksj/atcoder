cfdef969bd6e74ded7d1cc29550eb11a016d7f35
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-17 06:59:56 +0900

    A - Poor

---
+++ AtCoder Beginner Contest 155/A - Poor/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << ((a == b && b != c) || (b == c && c != a) || (c == a && a != b) ? "Yes" : "No") << endl;
+}
