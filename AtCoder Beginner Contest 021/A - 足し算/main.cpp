e16a6d20e7723a69658c1dd72fdc9f035e7abb78
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-23 09:21:02 +0900

    A - 足し算

---
+++ AtCoder Beginner Contest 021/A - 足し算/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int r = 0;
+    for (int i = 1; i <= n; i <<= 1) {
+        if ((n & i) > 0) {
+            r++;
+        }
+    }
+
+    cout << r << endl;
+    for (int i = 1; i <= n; i <<= 1) {
+        if ((n & i) > 0) {
+            cout << i << endl;
+        }
+    }
+}
