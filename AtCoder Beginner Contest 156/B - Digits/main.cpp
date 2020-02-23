5615711b0a732461c33cf29e14849e77b5e2680c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-23 09:07:30 +0900

    B - Digits

---
+++ AtCoder Beginner Contest 156/B - Digits/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+
+    int res = 1;
+    for (long long i = k; n > i - 1; i *= k) {
+        res++;
+    }
+
+    cout << res << endl;
+}
