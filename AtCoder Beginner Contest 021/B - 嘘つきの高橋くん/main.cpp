91497c6bf0a9847cbd47682ef625a78400818afa
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-24 08:19:53 +0900

    B - 嘘つきの高橋くん

---
+++ AtCoder Beginner Contest 021/B - 嘘つきの高橋くん/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n, a, b, k;
+    cin >> n >> a >> b >> k;
+
+    set<int> s;
+    s.insert(a);
+    s.insert(b);
+
+    for (int i = 0; i < k; i++) {
+        int p;
+        cin >> p;
+        s.insert(p);
+    }
+
+    cout << (static_cast<int>(s.size()) == k + 2 ? "YES" : "NO") << endl;
+}
