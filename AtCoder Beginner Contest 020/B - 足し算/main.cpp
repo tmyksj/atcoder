eecf0d262e11b6aef86118073c4160d34e4ea890
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-27 14:30:36 +0900

    B - 足し算

---
+++ AtCoder Beginner Contest 020/B - 足し算/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 10; ; i *= 10) {
+        if (b < i) {
+            cout << 2 * (i * a + b) << endl;
+            break;
+        }
+    }
+}
