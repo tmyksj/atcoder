5c045b4c3d18e5312259f03419effd020825212f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-02-29 11:36:52 +0900

    A - 高橋くんと年齢

---
+++ AtCoder Beginner Contest 019/A - 高橋くんと年齢/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> a(3);
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a.begin(), a.end());
+    cout << a[1] << endl;
+}
