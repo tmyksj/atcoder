a6b253977634f9cd88125a9afb00ba9cd1070c5a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-02 21:04:46 +0900

    C - 高橋くんと魔法の箱

---
+++ AtCoder Beginner Contest 019/C - 高橋くんと魔法の箱/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<long long> a;
+    for (int i = 0; i < n; i++) {
+        long long ai;
+        cin >> ai;
+        a.insert(ai);
+    }
+
+    int res = 0;
+    for (; !a.empty(); res++) {
+        for (long long i = *a.begin(); !a.empty() && i <= *a.rbegin(); i *= 2) {
+            a.erase(i);
+        }
+    }
+
+    cout << res << endl;
+}
