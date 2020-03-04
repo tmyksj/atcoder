1d7be8b81ae5c1a47d860c275d1f52e21ca63f82
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-04 15:58:08 +0900

    A - 豆まき

---
+++ AtCoder Beginner Contest 018/A - 豆まき/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a < b ? 1 : 0) + (a < c ? 1 : 0) + 1 << endl;
+    cout << (b < a ? 1 : 0) + (b < c ? 1 : 0) + 1 << endl;
+    cout << (c < a ? 1 : 0) + (c < b ? 1 : 0) + 1 << endl;
+}
