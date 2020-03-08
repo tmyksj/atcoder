784be646c1da3e68f8f455cf9563e7dce5058f52
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-08 11:00:47 +0900

    A - プロコン

---
+++ AtCoder Beginner Contest 017/A - プロコン/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int res = 0;
+    for (int i = 0; i < 3; i++) {
+        int s, e;
+        cin >> s >> e;
+        res += s * e / 10;
+    }
+    cout << res << endl;
+}
