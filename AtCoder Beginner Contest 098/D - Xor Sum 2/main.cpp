6142b05cee69e6e81b79621fe9ee4d580e204bd6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-08 11:42:56 +0900

    D - Xor Sum 2

---
+++ AtCoder Beginner Contest 098/D - Xor Sum 2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    long long res = 0;
+    for (int l = 0, r = 0, s = a[0]; l < n; l++) {
+        while (r + 1 < n && (s & a[r + 1]) == 0) {
+            s += a[r + 1];
+            r++;
+        }
+        s -= a[l];
+        res += r - l + 1;
+    }
+
+    cout << res << endl;
+}
