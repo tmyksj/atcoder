0f1c61c80e681635c616844eaf2d41dc823f2d17
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-11 20:33:28 +0900

    C - 高橋くんのバグ探し

---
+++ AtCoder Beginner Contest 015/C - 高橋くんのバグ探し/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool solve(int n, int k, vector<vector<int>>& t, int i, int c) {
+    if (i == n) {
+        return c == 0;
+    }
+
+    bool res = false;
+    for (int j = 0; j < k; j++) {
+        res = res || solve(n, k, t, i + 1, c ^ t[i][j]);
+    }
+
+    return res;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<vector<int>> t(n, vector<int>(k));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            cin >> t[i][j];
+        }
+    }
+
+    cout << (solve(n, k, t, 0, 0) ? "Found" : "Nothing") << endl;
+}
