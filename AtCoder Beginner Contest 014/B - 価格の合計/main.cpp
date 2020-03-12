b0f6e44996b47becf2f50d3405e3bfa2d29dec9c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-12 15:20:08 +0900

    B - 価格の合計

---
+++ AtCoder Beginner Contest 014/B - 価格の合計/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+
+        if ((x & (1 << i)) > 0) {
+            res += a;
+        }
+    }
+
+    cout << res << endl;
+}
