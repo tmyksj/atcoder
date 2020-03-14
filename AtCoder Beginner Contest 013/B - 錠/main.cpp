9ee01fe00f76d716d80a9d47a3a161cedcfadbe8
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-14 09:24:24 +0900

    B - 錠

---
+++ AtCoder Beginner Contest 013/B - 錠/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a < b) {
+        cout << min(b - a, a - b + 10) << endl;
+    } else {
+        cout << min(a - b, b - a + 10) << endl;
+    }
+}
