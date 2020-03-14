982f042e4a80a74d0f8dd564598f21c734e34e41
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-15 08:56:45 +0900

    B - Bishop

---
+++ パナソニックプログラミングコンテスト2020/B - Bishop/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long h, w;
+    cin >> h >> w;
+
+    if (h == 1 || w == 1) {
+        cout << "1" << endl;
+    } else {
+        cout << ((h + 1) / 2 + h / 2) * (w / 2) + ((h + 1) / 2) * (w % 2) << endl;
+    }
+}
