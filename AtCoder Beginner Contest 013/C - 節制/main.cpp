813e8d58f8423e2375ff166754778351ccda2cc0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-16 21:05:10 +0900

    C - 節制

---
+++ AtCoder Beginner Contest 013/C - 節制/main.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n, h, a, b, c, d, e;
+    cin >> n >> h >> a >> b >> c >> d >> e;
+
+    long long res = 1LL << 61;
+    for (int i = 0; i <= n; i++) {
+        long long j = (n * e - h - (b + e) * i + d + e) / (d + e);
+        if (i + j <= n) {
+            res = min(res, a * i + c * max(j, 0LL));
+        }
+    }
+
+    cout << res << endl;
+}
