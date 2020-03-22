b4f4302b8592ceb3c3e099f7a290b7b5d0792155
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-23 06:47:33 +0900

    C - Maximum Volume

---
+++ AtCoder Beginner Contest 159/C - Maximum Volume/main.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int l;
+    cin >> l;
+
+    double w = static_cast<double>(l) / 3;
+    cout << setprecision(12) << fixed << w * w * w << endl;
+}
