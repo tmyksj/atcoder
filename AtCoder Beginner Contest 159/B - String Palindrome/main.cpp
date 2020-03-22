ff187ce32bed2868fdb683d402eb6d051a264004
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-03-23 06:47:30 +0900

    B - String Palindrome

---
+++ AtCoder Beginner Contest 159/B - String Palindrome/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_palindrome(string s, int b, int e) {
+    bool res = true;
+    for (int i = b; i <= e; i++) {
+        res = res && s[i] == s[e - i];
+    }
+    return res;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    bool res = is_palindrome(s, 0, n - 1);
+    res = res && is_palindrome(s, 0, n / 2 - 1);
+    res = res && is_palindrome(s, n / 2 + 1, n - 1);
+
+    cout << (res ? "Yes" : "No") << endl;
+}
