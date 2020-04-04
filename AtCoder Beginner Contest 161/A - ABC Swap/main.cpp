d6ad0ceab95da74e18603af806422558085acc18
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-04 22:42:39 +0900

    A - ABC Swap

---
+++ AtCoder Beginner Contest 161/A - ABC Swap/main.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    swap(x, y);
+    swap(x, z);
+
+    cout << x << " " << y << " " << z << endl;
+}
