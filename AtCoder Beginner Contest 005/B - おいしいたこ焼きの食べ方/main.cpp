ae08cab11406afc843a20a787c7cbf2f9c45b991
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-04 09:24:23 +0900

    B - おいしいたこ焼きの食べ方

---
+++ AtCoder Beginner Contest 005/B - おいしいたこ焼きの食べ方/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> t(n);
+    for (int i = 0; i < n; i++) {
+        cin >> t[i];
+    }
+
+    cout << *min_element(t.begin(), t.end()) << endl;
+}
