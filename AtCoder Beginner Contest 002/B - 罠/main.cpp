ab338c347dcf89c6b6a84861929b9e3f6097bf43
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-10 07:54:16 +0900

    B - 罠

---
+++ AtCoder Beginner Contest 002/B - 罠/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string w;
+    cin >> w;
+
+    for (int i = 0; i < static_cast<int>(w.size()); i++) {
+        if (w[i] != 'a' && w[i] != 'i' && w[i] != 'u' && w[i] != 'e' && w[i] != 'o') {
+            cout << w[i];
+        }
+    }
+    cout << endl;
+}
