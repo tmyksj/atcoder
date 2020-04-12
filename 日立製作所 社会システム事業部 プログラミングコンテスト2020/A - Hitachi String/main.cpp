d4ff956fdee58810a3e38eb6715c89bad17e6cf8
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-12 10:40:21 +0900

    A - Hitachi String

---
+++ 日立製作所 社会システム事業部 プログラミングコンテスト2020/A - Hitachi String/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    bool res = s.size() % 2 == 0;
+    for (int i = 0; i < static_cast<int>(s.size()); i += 2) {
+        if (s[i] != 'h' || s[i + 1] != 'i') {
+            res = false;
+        }
+    }
+
+    cout << (res ? "Yes" : "No") << endl;
+}
