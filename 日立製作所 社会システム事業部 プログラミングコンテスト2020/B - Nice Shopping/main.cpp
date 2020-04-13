bac2dcb0930eea069ffdadf12e5426b99c1270f4
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-13 12:36:07 +0900

    B - Nice Shopping

---
+++ 日立製作所 社会システム事業部 プログラミングコンテスト2020/B - Nice Shopping/main.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int a, b, m;
+    cin >> a >> b >> m;
+
+    vector<int> ai(a);
+    for (int i = 0; i < a; i++) {
+        cin >> ai[i];
+    }
+
+    vector<int> bi(b);
+    for (int i = 0; i < b; i++) {
+        cin >> bi[i];
+    }
+
+    int res = *min_element(ai.begin(), ai.end()) + *min_element(bi.begin(), bi.end());
+    for (int i = 0; i < m; i++) {
+        int x, y, c;
+        cin >> x >> y >> c;
+        res = min(res, ai[x - 1] + bi[y - 1] - c);
+    }
+
+    cout << res << endl;
+}
