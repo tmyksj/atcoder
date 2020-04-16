2a67aa2233567eca3aa929b5cb11ae071c32e269
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-16 14:00:52 +0900

    D - 重複検査

---
+++ 第一回 アルゴリズム実技検定 過去問/D - 重複検査/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> c(n, 0);
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        c[a - 1]++;
+    }
+
+    vector<int>::iterator x = find(c.begin(), c.end(), 0);
+    vector<int>::iterator y = find(c.begin(), c.end(), 2);
+
+    if (x == c.end()) {
+        cout << "Correct" << endl;
+    } else {
+        cout << y - c.begin() + 1 << " " << x - c.begin() + 1 << endl;
+    }
+}
