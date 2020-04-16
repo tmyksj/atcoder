32b8395ca1bd55ea6e07c7c7e8be25623d8a4383
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-16 13:50:25 +0900

    A - 2 倍チェック

---
+++ 第一回 アルゴリズム実技検定 過去問/A - 2 倍チェック/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if ('0' <= s[0] && s[0] <= '9'
+            && '0' <= s[1] && s[1] <= '9'
+            && '0' <= s[2] && s[2] <= '9') {
+        cout << 2 * (100 * (s[0] - '0') + 10 * (s[1] - '0') + (s[2] - '0')) << endl;
+    } else {
+        cout << "error" << endl;
+    }
+}
