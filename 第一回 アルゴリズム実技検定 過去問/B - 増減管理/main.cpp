20bb645fd1a9063b590685fd94c6c2ddc514e015
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-16 13:53:27 +0900

    B - 増減管理

---
+++ 第一回 アルゴリズム実技検定 過去問/B - 増減管理/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a_prev;
+    cin >> a_prev;
+
+    for (int i = 1; i < n; i++) {
+        int a;
+        cin >> a;
+
+        if (a_prev < a) {
+            cout << "up " << a - a_prev << endl;
+        } else if (a_prev == a) {
+            cout << "stay" << endl;
+        } else {
+            cout << "down " << a_prev - a << endl;
+        }
+
+        a_prev = a;
+    }
+}
