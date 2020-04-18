72e426b4e0ef4280cdf5ae2b3b398a29f0a75e58
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-19 08:37:34 +0900

    A - DDCC Finals

---
+++ DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選/A - DDCC Finals/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << (100000 * (max(0, 4 - x) + max(0, 4 - y)) + (x == 1 && y == 1 ? 400000 : 0)) << endl;
+}
