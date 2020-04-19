bf10888149eb76abf4874ad3866f1040b33cf574
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-20 08:58:36 +0900

    C - management

---
+++ AtCoder Beginner Contest 163/C - management/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> res(n, 0);
+    for (int i = 1; i < n; i++) {
+        int a;
+        cin >> a;
+        res[a - 1]++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << res[i] << endl;
+    }
+}
