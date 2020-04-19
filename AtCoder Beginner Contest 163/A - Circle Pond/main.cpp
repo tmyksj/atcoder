b3f565c327c1c1f5a9965c68352b5290c5b0674b
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-20 08:58:30 +0900

    A - Circle Pond

---
+++ AtCoder Beginner Contest 163/A - Circle Pond/main.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const double pi = 3.14159265358979323846264338327950288;
+
+    int r;
+    cin >> r;
+    cout << setprecision(12) << fixed << 2 * pi * r << endl;
+}
