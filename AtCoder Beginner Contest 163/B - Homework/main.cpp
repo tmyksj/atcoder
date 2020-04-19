aed33bdecdfa34adedad800dbc9ae20b4f406dad
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-20 08:58:33 +0900

    B - Homework

---
+++ AtCoder Beginner Contest 163/B - Homework/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    long long n, m;
+    cin >> n >> m;
+
+    vector<long long> a(m);
+    for (int i = 0; i < m; i++) {
+        cin >> a[i];
+    }
+
+    cout << max(-1LL, n - accumulate(a.begin(), a.end(), 0LL)) << endl;
+}
