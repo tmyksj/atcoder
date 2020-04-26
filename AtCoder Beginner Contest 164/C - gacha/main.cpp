10f0ca4d3fbae9a6f29e27d15e180905ab1729e7
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-26 22:42:45 +0900

    C - gacha

---
+++ AtCoder Beginner Contest 164/C - gacha/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<string> st;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        st.insert(s);
+    }
+
+    cout << st.size() << endl;
+}
