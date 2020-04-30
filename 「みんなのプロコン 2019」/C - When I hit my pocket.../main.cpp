feb868fa1075ee652d3350f57a13659f5799535e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-04-30 16:55:05 +0900

    C - When I hit my pocket...

---
+++ 「みんなのプロコン 2019」/C - When I hit my pocket.../main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long k, a, b;
+    cin >> k >> a >> b;
+
+    if (b - a > 2 && k - a + 1 >= 0) {
+        cout << a + (b - a) * ((k - a + 1) / 2) + ((k - a + 1) % 2 == 1 ? 1 : 0) << endl;
+    } else {
+        cout << k + 1 << endl;
+    }
+}
