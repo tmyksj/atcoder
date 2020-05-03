9e308fddfcf3c22b61ce0d53113efd17966f2582
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-03 22:41:13 +0900

    B - Trick or Treat

---
+++ AtCoder Beginner Contest 166/B - Trick or Treat/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<set<int>> s(n, set<int>());
+    for (int i = 0; i < k; i++) {
+        int d;
+        cin >> d;
+
+        for (int j = 0; j < d; j++) {
+            int a;
+            cin >> a;
+            s[a - 1].insert(i);
+        }
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i].size() == 0) {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
