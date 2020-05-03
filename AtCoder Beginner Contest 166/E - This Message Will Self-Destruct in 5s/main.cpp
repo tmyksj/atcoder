c3f9e36fe8702d969a15e6c550fbc4a534bdb077
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-03 22:41:40 +0900

    E - This Message Will Self-Destruct in 5s

---
+++ AtCoder Beginner Contest 166/E - This Message Will Self-Destruct in 5s/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<long long> m(n, 0);
+    for (int i = 0; i < n; i++) {
+        if (i + a[i] < n) {
+            m[i + a[i]]++;
+        }
+    }
+
+    long long res = 0;
+    for (int i = 0; i < n; i++) {
+        if (i - a[i] >= 0) {
+            res += m[i - a[i]];
+        }
+    }
+
+    cout << res << endl;
+}
