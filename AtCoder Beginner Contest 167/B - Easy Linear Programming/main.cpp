21bcc50ac1a50bb1043df745a8f2fc67958268f5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-11 09:06:51 +0900

    B - Easy Linear Programming

---
+++ AtCoder Beginner Contest 167/B - Easy Linear Programming/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, k;
+    cin >> a >> b >> c >> k;
+
+    if (a + b >= k) {
+        cout << min(a, k) << endl;
+    } else {
+        cout << 2 * a + b - k << endl;
+    }
+}
