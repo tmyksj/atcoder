d2cf4522dbdd338a2a7e03def31f94145f060783
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-20 19:04:08 +0900

    B - ... (Triple Dots)

---
+++ AtCoder Beginner Contest 168/B - ... (Triple Dots)/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int k;
+    string s;
+    cin >> k >> s;
+    cout << (static_cast<int>(s.size()) <= k ? s : s.substr(0, k) + "...") << endl;
+}
