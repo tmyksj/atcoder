199fee76d6b47d2bf69b4c600b2f965686614018
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-29 18:17:46 +0900

    D - Digit Sum Replace

---
+++ DISCO presents ディスカバリーチャンネル コードコンテスト2020 予選/D - Digit Sum Replace/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m;
+    cin >> m;
+
+    long long r = 0, s = 0;
+    for (int i = 0; i < m; i++) {
+        long long d, c;
+        cin >> d >> c;
+        r += c;
+        s += d * c;
+    }
+
+    cout << r - 1 + (s - 1) / 9 << endl;
+}
