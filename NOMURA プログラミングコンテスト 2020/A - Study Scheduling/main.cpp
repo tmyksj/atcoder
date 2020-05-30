49b24c1509a74a022ca88cbe32ddf522964d1893
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-31 08:36:08 +0900

    A - Study Scheduling

---
+++ NOMURA プログラミングコンテスト 2020/A - Study Scheduling/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int h1, m1, h2, m2, k;
+    cin >> h1 >> m1 >> h2 >> m2 >> k;
+    cout << (60 * h2 + m2) - (60 * h1 + m1) - k << endl;
+}
