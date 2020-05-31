4a08387df3aae3991fbb4396ac95dd6e3a135566
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-31 22:42:36 +0900

    E - Count Median

---
+++ AtCoder Beginner Contest 169/E - Count Median/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    if (n % 2 == 0) {
+        cout << (b[n / 2] + b[n / 2 - 1]) - (a[n / 2] + a[n / 2 - 1]) + 1 << endl;
+    } else {
+        cout << b[(n - 1) / 2] - a[(n - 1) / 2] + 1 << endl;
+    }
+}
