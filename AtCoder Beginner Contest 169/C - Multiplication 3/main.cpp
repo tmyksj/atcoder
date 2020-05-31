d6d14d6b9109597a71f4a0097da11ab336336c28
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-05-31 22:42:00 +0900

    C - Multiplication 3

---
+++ AtCoder Beginner Contest 169/C - Multiplication 3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    long long a;
+    string b;
+    cin >> a >> b;
+    cout << a * (100 * (b[0] - '0') + 10 * (b[2] - '0') + (b[3] - '0')) / 100 << endl;
+}
