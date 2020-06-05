89ea4e4b1aec7b689351258d7dfd632bb3fa0240
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-05 19:45:32 +0900

    B - Contests

---
+++ エイシング プログラミング コンテスト 2019/B - Contests/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int q = 0, r = 0, s = 0;
+    for (int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+
+        if (p <= a) {
+            q++;
+        } else if (p <= b) {
+            r++;
+        } else {
+            s++;
+        }
+    }
+
+    cout << min({ q, r, s }) << endl;
+}
