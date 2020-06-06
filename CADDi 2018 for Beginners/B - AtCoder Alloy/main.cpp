3f2cc145b9969a435e3c1d48fc806a88e53ab616
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-06 09:51:55 +0900

    B - AtCoder Alloy

---
+++ CADDi 2018 for Beginners/B - AtCoder Alloy/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, h, w;
+    cin >> n >> h >> w;
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a >= h && b >= w) {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
