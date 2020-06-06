30cbbf30f194dfd8677e61629414ec8878a7aa3c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-06 09:48:05 +0900

    A - 12/22

---
+++ CADDi 2018 for Beginners/A - 12/22/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+
+    map<char, int> m;
+    for (int i = 0; i < static_cast<int>(n.size()); i++) {
+        m[n[i]]++;
+    }
+
+    cout << m['2'] << endl;
+}
