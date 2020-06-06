e598d385b2199b1fcc213b7f3b9c74aacdcefe11
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-06 09:59:29 +0900

    D - Harlequin

---
+++ CADDi 2018 for Beginners/D - Harlequin/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool r = false;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        r = r || a % 2 != 0;
+    }
+
+    cout << (r ? "first" : "second") << endl;
+}
