86f5c98c99cdb83e3eac09e6551e6d107c33dad4
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-15 08:18:48 +0900

    A - Five Variables

---
+++ AtCoder Beginner Contest 170/A - Five Variables/main.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> x(5);
+    for (int i = 0; i < 5; i++) {
+        cin >> x[i];
+    }
+
+    cout << find(x.begin(), x.end(), 0) - x.begin() + 1 << endl;
+}
