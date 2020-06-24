52eff3c7be383beb2ed05562e7a16dafcb270081
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-24 16:59:10 +0900

    A - Measure

---
+++ Tenka1 Programmer Beginner Contest/A - Measure/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s.size() == 3) {
+        reverse(s.begin(), s.end());
+    }
+
+    cout << s << endl;
+}
