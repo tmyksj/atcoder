abf66ca470a6c4ab0dfa9d8630d0750620db5a77
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-27 22:41:33 +0900

    D - Sum of Divisors

---
+++ AtCoder Beginner Contest 172/D - Sum of Divisors/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> f(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j += i) {
+            f[j]++;
+        }
+    }
+
+    long long res = 0;
+    for (int i = 1; i <= n; i++) {
+        res += i * f[i];
+    }
+
+    cout << res << endl;
+}
