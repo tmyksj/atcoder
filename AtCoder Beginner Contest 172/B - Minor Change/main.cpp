80c4bd9f5c835a379f67f4ae73aabc764e26e0af
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-06-27 22:41:28 +0900

    B - Minor Change

---
+++ AtCoder Beginner Contest 172/B - Minor Change/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    int res = 0;
+    for (int i = 0; i < static_cast<int>(s.size()); i++) {
+        if (s[i] != t[i]) {
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
