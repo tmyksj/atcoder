6637074180a38cee77c281b3c861d4adc432627e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-07-18 09:32:09 +0900

    A - みんなでワイワイみかん

---
+++ AtCoder Regular Contest 056/A - みんなでワイワイみかん/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long a, b, k, l;
+    cin >> a >> b >> k >> l;
+    cout << min({ a * k, a * (k % l) + b * (k / l), b * (k / l + 1) }) << endl;
+}
