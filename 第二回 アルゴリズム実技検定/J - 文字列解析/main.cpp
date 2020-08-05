828330967203c5b8b99ca1d8f3d70bcaa2f4c10d
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-05 17:56:50 +0900

    J - 文字列解析

---
+++ 第二回 アルゴリズム実技検定/J - 文字列解析/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    stack<string> st;
+    st.push("");
+    for (int i = 0; i < static_cast<int>(s.size()); i++) {
+        if (s[i] == '(') {
+            st.push("");
+        } else if (s[i] == ')') {
+            string t = st.top(); st.pop();
+            string u = st.top(); st.pop();
+
+            string v = t;
+            reverse(v.begin(), v.end());
+
+            st.push(u + t + v);
+        } else {
+            string t = st.top(); st.pop();
+            st.push(t + s[i]);
+        }
+    }
+
+    cout << st.top() << endl;
+}
