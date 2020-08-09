b4b679796542313f206460bbd28c1cffd2ad64d6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-10 08:27:20 +0900

    D - Road to Millionaire

---
+++ M-SOLUTIONS プロコンオープン 2020/D - Road to Millionaire/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    long long res = 1000;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] < a[i + 1]) {
+            res = res / a[i] * a[i + 1] + res % a[i];
+        }
+    }
+
+    cout << res << endl;
+}
