23fe3f30e1c126385476451a398432c2ea5c0aad
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-13 08:29:39 +0900

    C - Repsept

---
+++ AtCoder Beginner Contest 174/C - Repsept/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    long long k;
+    cin >> k;
+
+    vector<bool> m(k, false);
+    long long res = 1;
+    for (long long i = 1, j = 7 % k; !m[j]; i *= 10, i %= k, j += 7 * i, j %= k) {
+        m[j] = true;
+        if (j == 0) {
+            break;
+        }
+
+        res++;
+    }
+
+    cout << (m[0] ? res : -1) << endl;
+}
