39c050f07ef1c2e32418f7e1597abbdbc8656314
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-14 08:16:48 +0900

    D - Alter Altar

---
+++ AtCoder Beginner Contest 174/D - Alter Altar/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string c;
+    cin >> n >> c;
+    cout << count(c.begin(), c.begin() + count(c.begin(), c.end(), 'R'), 'W') << endl;
+}
