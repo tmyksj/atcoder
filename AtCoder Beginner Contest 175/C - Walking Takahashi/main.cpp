27770c13a1d24ab3aa9dd62e363fd78edb985a6f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-19 08:11:29 +0900

    C - Walking Takahashi

---
+++ AtCoder Beginner Contest 175/C - Walking Takahashi/main.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long x, k, d;
+    cin >> x >> k >> d;
+
+    if ((k - min(k, abs(x) / d)) % 2 == 0) {
+        cout << abs(x) - min(k, abs(x) / d) * d << endl;
+    } else {
+        cout << d - abs(x) + min(k, abs(x) / d) * d << endl;
+    }
+}
