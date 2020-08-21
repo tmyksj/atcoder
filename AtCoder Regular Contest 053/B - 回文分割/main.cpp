a090f3040bbf1a6729b152fbbc81d87f5aaff512
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-22 07:26:56 +0900

    B - 回文分割

---
+++ AtCoder Regular Contest 053/B - 回文分割/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    map<int, int> c;
+    for (int i = 0; i < static_cast<int>(s.size()); i++) {
+        c[s[i]]++;
+    }
+
+    int a = 0, b = 0;
+    for (map<int, int>::iterator i = c.begin(); i != c.end(); i++) {
+        a += i->second / 2;
+        b += i->second % 2;
+    }
+
+    cout << (b == 0 ? 2 * a : 2 * (a / b) + 1) << endl;
+}
