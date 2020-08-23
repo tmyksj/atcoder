780f5d79f54e82a1babcb8ffe118605c70cd08b0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-08-24 08:38:38 +0900

    B - Multiple of 9

---
+++ AtCoder Beginner Contest 176/B - Multiple of 9/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string n;
+    cin >> n;
+
+    int s = 0;
+    for (int i = 0; i < static_cast<int>(n.size()); i++) {
+        s += n[i] - '0';
+    }
+
+    cout << (s % 9 == 0 ? "Yes" : "No") << endl;
+}
