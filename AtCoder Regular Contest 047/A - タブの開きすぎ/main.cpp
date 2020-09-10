53088d2f624f7a3544d4359366b8cf32fdf4e3be
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-09-10 13:41:31 +0900

    A - タブの開きすぎ

---
+++ AtCoder Regular Contest 047/A - タブの開きすぎ/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, l;
+    string s;
+    cin >> n >> l >> s;
+
+    int res = 0, c = 1;
+    for (int i = 0; i < n; i++) {
+        c += (s[i] == '+' ? 1 : -1);
+        if (c > l) {
+            c = 1;
+            res++;
+        }
+    }
+
+    cout << res << endl;
+}
