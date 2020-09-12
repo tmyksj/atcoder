67a2fe85adf88818a9d68aed111f1f1f46dae202
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-09-13 07:43:13 +0900

    A - スペース高橋君

---
+++ AtCoder Regular Contest 045/A - スペース高橋君/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> res(0);
+    for (string s; cin >> s; ) {
+        if (s == "Left") {
+            res.push_back("<");
+        } else if (s == "Right") {
+            res.push_back(">");
+        } else {
+            res.push_back("A");
+        }
+    }
+
+    for (int i = 0; i < static_cast<int>(res.size()); i++) {
+        cout << (i == 0 ? "" : " ") << res[i];
+    }
+    cout << endl;
+}
