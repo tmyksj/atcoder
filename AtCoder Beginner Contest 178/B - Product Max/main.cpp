711a8c18a4005e0d40c12d2721ffd250896b6ce0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-09-16 08:53:21 +0900

    B - Product Max

---
+++ AtCoder Beginner Contest 178/B - Product Max/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if ((a < 0 && 0 < b) || (c < 0 && 0 < d)) {
+        cout << max({ a * c, a * d, b * c, b * d, 0LL }) << endl;
+    } else {
+        cout << max({ a * c, a * d, b * c, b * d }) << endl;
+    }
+}
