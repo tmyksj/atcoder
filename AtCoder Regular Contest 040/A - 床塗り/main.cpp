f0fb489273af6a42b1510ed547b6506a3709b985
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-10-05 08:21:37 +0900

    A - 床塗り

---
+++ AtCoder Regular Contest 040/A - 床塗り/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        for (int j = 0; j < n; j++) {
+            m[s[j]]++;
+        }
+    }
+
+    cout << (m['R'] < m['B'] ? "AOKI" : (m['R'] == m['B'] ? "DRAW" : "TAKAHASHI")) << endl;
+}
