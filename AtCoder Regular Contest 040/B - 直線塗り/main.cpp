d949fc69e7befeb80942ec4bb4d8836f449d5faf
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-10-06 08:32:01 +0900

    B - 直線塗り

---
+++ AtCoder Regular Contest 040/B - 直線塗り/main.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, r;
+    string s;
+    cin >> n >> r >> s;
+
+    int a = 0, b = 0;
+    for (int i = n - 1; i >= 0; ) {
+        if (s[i] == '.') {
+            a = max(a, i - r + 1);
+            b++;
+            i -= r;
+        } else {
+            i--;
+        }
+    }
+
+    cout << a + b << endl;
+}
