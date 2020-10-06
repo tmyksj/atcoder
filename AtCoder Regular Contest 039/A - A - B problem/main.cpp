09dc8df5ee505f21c56840c9776d64e6dbcf60eb
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-10-07 08:20:35 +0900

    A - A - B problem

---
+++ AtCoder Regular Contest 039/A - A - B problem/main.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << max({ a - b + 100 * (9 - a / 100),
+            a - b + 10 * (9 - a / 10 % 10),
+            a - b + 9 - a % 10,
+            a - b + 100 * (b / 100 - 1),
+            a - b + 10 * (b / 10 % 10),
+            a - b + b % 10 }) << endl;
+}
