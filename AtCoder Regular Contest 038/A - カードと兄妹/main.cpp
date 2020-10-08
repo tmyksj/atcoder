73dcf9233c5d593702cc69f414071f07f8e9e23a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-10-09 08:06:03 +0900

    A - カードと兄妹

---
+++ AtCoder Regular Contest 038/A - カードと兄妹/main.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    int res = 0;
+    for (int i = 0; i < n; i += 2) {
+        res += a[i];
+    }
+
+    cout << res << endl;
+}
