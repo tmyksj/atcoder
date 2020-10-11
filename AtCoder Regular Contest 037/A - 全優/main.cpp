6cf13eb6ddd3401013d630760f9f52ef84483546
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-10-11 09:06:04 +0900

    A - 全優

---
+++ AtCoder Regular Contest 037/A - 全優/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m;
+        res += max(0, 80 - m);
+    }
+
+    cout << res << endl;
+}
