c999d2120604007300985fba4db07737519088c2
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-11-06 08:11:28 +0900

    A - ダイナミックなポーズ

---
+++ AtCoder Regular Contest 026/A - ダイナミックなポーズ/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n >> a >> b;
+    cout << max(n - 5, 0) * a + min(n, 5) * b << endl;
+}
