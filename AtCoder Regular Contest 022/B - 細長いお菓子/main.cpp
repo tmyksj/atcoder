0256ba98eaed894251c94842e78d66b06125435c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-11-15 09:19:41 +0900

    B - 細長いお菓子

---
+++ AtCoder Regular Contest 022/B - 細長いお菓子/main.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int res = 0;
+
+    vector<int> b(*max_element(a.begin(), a.end()) + 1, -1);
+    for (int i = 0, j = -1; i < n; i++) {
+        j = max(j, b[a[i]]);
+        res = max(res, i - j);
+        b[a[i]] = i;
+    }
+
+    cout << res << endl;
+}
