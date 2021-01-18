9484d14eae3da9189a0aa7b07f6dc69fca53af33
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-01-18 09:38:45 +0900

    A - ABC Preparation

---
+++ AtCoder Beginner Contest 185/A - ABC Preparation/main.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a1, a2, a3, a4;
+    cin >> a1 >> a2 >> a3 >> a4;
+    cout << min({ a1, a2, a3, a4 }) << endl;
+}
