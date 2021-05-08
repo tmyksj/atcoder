6b6ed17015dfcb714a7db094a11c72a316251aef
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-05-09 08:14:11 +0900

    B - Intersection

---
+++ AtCoder Beginner Contest 199（Sponsored by Panasonic）/B - Intersection/main.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+    cout << max(*min_element(b.begin(), b.end()) - *max_element(a.begin(), a.end()) + 1, 0) << endl;
+}
