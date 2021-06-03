3432ee085fcdc948e28af7f2e190e573b5fe6a0a
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-06-04 07:42:30 +0900

    A - Chinchirorin

---
+++ AtCoder Beginner Contest 203（Sponsored by Panasonic）/A - Chinchirorin/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a == b) {
+        cout << c << endl;
+    } else if (a == c) {
+        cout << b << endl;
+    } else if (b == c) {
+        cout << a << endl;
+    } else {
+        cout << "0" << endl;
+    }
+}
