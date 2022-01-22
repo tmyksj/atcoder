0e9d13500458397dc36e03b412c4ef64b0f554ad
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-01-22 19:52:22 +0900

    003 - Sum of N Integers

---
+++ アルゴリズムと数学　演習問題集/003 - Sum of N Integers/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cout << accumulate(a.begin(), a.end(), 0) << endl;
+}
