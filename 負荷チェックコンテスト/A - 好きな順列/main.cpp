23eb08cbd1d3d724df15f238e9f1582f6f772445
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-02-13 20:00:08 +0900

    A - 好きな順列

---
+++ 負荷チェックコンテスト/A - 好きな順列/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int res[] = {
+        2, 20, 19, 10, 17,
+        12, 9, 13, 14, 15,
+        4, 8, 3, 16, 11,
+        18, 7, 5, 1, 6,
+    };
+
+    for (int i = 0; i < 20; i++) {
+        cout << res[i] << endl;
+    }
+}
