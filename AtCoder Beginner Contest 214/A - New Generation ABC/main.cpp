29d560d708343fe51aa96ef2b7c04adc7ce09dcf
tmyksj <33417830+tmyksj@users.noreply.github.com>
2024-08-04 19:56:20 +0900

    A - New Generation ABC

---
+++ AtCoder Beginner Contest 214/A - New Generation ABC/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n <= 125) {
+        cout << "4" << endl;
+    } else if (n <= 211) {
+        cout << "6" << endl;
+    } else {
+        cout << "8" << endl;
+    }
+}
